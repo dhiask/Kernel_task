e12886a9cbc7f28e71f7b1e145def2fe2fbe1c40
dhiask <137756202+dhiask@users.noreply.github.com>
2023-06-26 09:48:41 +0200

    Kernel_Log.c
    Main data

--- my_kernel_module/Kernel_Log.c
+++ my_kernel_module/Kernel_Log.c
@@ -1,14 +1,15 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include "Source.c"
+#include "Source.h"
 
 int init_module(void)
 {
-    printk(KERN_INFO "Hello, kernel module!\n");
-    return 0;
+    printk(KERN_INFO "Hello, kernel module fährt hoch!\n");
+    return my_module_init();
 }
 
 void cleanup_module(void)
 {
-    printk(KERN_INFO "Goodbye, kernel module!\n");
+    printk(KERN_INFO "Goodbye, kernel module fährt runter !\n");
+    my_module_exit();
 }
